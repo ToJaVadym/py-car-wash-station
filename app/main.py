ca8ab908bb5fc9f28e9c2523a0dd41015ea61674
ToJaVadym <vadymivaneiko2@gmail.com>
2026-03-11 22:56:08 +0100

    Deleted comments

--- app/main.py
+++ app/main.py
@@ -14,7 +14,6 @@ class CarWashStation:
 
     def calculate_washing_price(self, car):
         """Calculates the price for a potential wash."""
-        # Cost = comfort * (diff in cleanness) * rating / distance
         price = (car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating /
                  self.distance_from_city_center)
         return round(price, 1)
@@ -29,7 +28,6 @@ class CarWashStation:
         total_income = 0.0
         for car in cars:
             if car.clean_mark < self.clean_power:
-                # Calculate price before washing to get the correct difference
                 total_income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
 
